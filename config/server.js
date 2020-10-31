ba16f722a685d7310cd9a5d92f9feb6071adab3a
Eloise Boyer <eloise04@live.fr>
2020-10-31 12:54:48 +0100

    first push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '47b36a468267ba8ce8623840ce02e783'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '43a26548-a889-4673-baf4-b3efcd0c33aa'
+};
